917002351ef12ede8c89dd87a45226a2bf16b4c7
Aru <aruncr86@gmail.com>
2013-07-16 17:44:18 +0000

    web6

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buff = Buffer(128);
+var buf = new Buffer(128);
 app.get('/', function(request, response) {
 buff= fs.readFileSync("./index.html");
 response.send(buf.toString(buff)) ;
