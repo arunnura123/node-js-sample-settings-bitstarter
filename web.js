f476e19756e3b13f45cad30882d93d371dbda58b
Aru <aruncr86@gmail.com>
2013-07-16 17:40:18 +0000

    web5

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buff = Buffer(128);
 app.get('/', function(request, response) {
-fs.readFile('index.html', 'utf8',function read(err, data) {
-    if (err) {
-        throw err;
-    }  
-  response.send('hello');
+buff= fs.readFileSync("./index.html");
+response.send(buf.toString(buff)) ;
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
