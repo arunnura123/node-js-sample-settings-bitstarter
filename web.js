958d0570ffcffe66b6203f463349073ec0f5d929
Aru <aruncr86@gmail.com>
2013-07-16 18:21:06 +0000

    web11

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express.createServer(express.logger());
 var buff = Buffer(128);
 var buf = new Buffer(128);
 app.get('/', function(request, response) {
-buff=fs.readFileSync("./index.html");
-buffer.write(buff,"utf-8");
-response.send(buffer.toString("utf-8", 0, 32));
+buf=fs.readFileSync("./index.html");
+buff.write(buf,"utf-8");
+response.send(buff.toString("utf-8", 0, 32));
 } );
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
