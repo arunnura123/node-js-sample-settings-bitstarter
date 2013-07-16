711fbc979b4f3f46679274d5d33cc927a1c7ad5e
Aru <aruncr86@gmail.com>
2013-07-16 17:04:42 +0000

    web

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var mydata;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   
+fs.readFileSync('index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
