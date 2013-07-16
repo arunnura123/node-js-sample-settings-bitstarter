3605c425f82d0551abb5e25fb3ef39859758f1f2
Aru <aruncr86@gmail.com>
2013-07-16 17:48:13 +0000

    web8

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 var buff = Buffer(128);
 var buf = new Buffer(128);
 app.get('/', function(request, response) {
-buff= fs.readFileSync("./index.html",'utf8');
-response.send(buf.toString(buff)) ;
+buff=fs.readFileSync("./index.html");
+response.send(buff) ;
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
