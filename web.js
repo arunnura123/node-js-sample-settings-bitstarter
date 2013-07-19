e678b513907eb8d78399ac69f83e650c47f90295
Aru <aruncr86@gmail.com>
2013-07-19 10:19:42 +0000

    web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
 buf=fs.readFileSync("./index.html","utf-8");
 response.send(buf.toString("utf-8", 0, 32));
 } );
-var port = process.env.PORT || 5000;
+var port = process.env.PORT ||  8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
