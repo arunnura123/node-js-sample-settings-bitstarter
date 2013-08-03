26b6ca36b5e07bb8ea583143fda9a6180781fd78
Aru <aruncr86@gmail.com>
2013-08-03 19:49:31 +0000

    m

--- web.js
+++ web.js
@@ -14,19 +14,3 @@ app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-function WriteToFile(element) {
- console.log("hello");
- var array = new Array();
-    array[0] = "Red";
-    array[1] = "Blue";
-    array[3] = "Green";
-
-    var ul = document.createElement("ul");
-    for (var i=0; i < array.length; i++) {
-       var li = document.createElement("li");
-       li.innerHtml = array[i];
-       ul.appendChild(li);
-    }
-    body.insertAfter(ul, element);
-}
-
