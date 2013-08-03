1fb8792818bbbc9603372811ad10d453713f5cbc
Aru <aruncr86@gmail.com>
2013-08-03 18:49:16 +0000

    m

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
@@ -6,8 +7,26 @@ var buf = new Buffer(128);
 app.get('/', function(request, response) {
 buf=fs.readFileSync("./index.html","utf-8");
 response.send(buf.toString("utf-8", 0, 32));
+ console.log("helo");
 } );
 var port = process.env.PORT ||  8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function WriteToFile(element) {
+ console.log("hello");
+ var array = new Array();
+    array[0] = "Red";
+    array[1] = "Blue";
+    array[3] = "Green";
+
+    var ul = document.createElement("ul");
+    for (var i=0; i < array.length; i++) {
+       var li = document.createElement("li");
+       li.innerHtml = array[i];
+       ul.appendChild(li);
+    }
+    body.insertAfter(ul, element);
+}
+
